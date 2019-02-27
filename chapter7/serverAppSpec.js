a5d0c5e4681b386cd4e4057bda5b68899c4e858f
Guilherme <tec.guilherme.dsc@gmail.com>
2019-02-27 16:55:45 -0300

    chapter 7

---
+++ chapter7/serverAppSpec.js
@@ -0,0 +1,33 @@
+describe('MainCtrl Server Calls', function() {
+    beforeEach(module('serverApp'));
+
+    var ctrl, mockBackend;
+
+    beforeEach(inject(function($controller, $httpBackend) {
+        mockBackend = $httpBackend;
+        mockBackend.expectGET('/api/note')
+            .respond([{id: 1, label: 'Mock'}]);
+        ctrl = $controller('MainCtrl')
+        // Neste ponto uma solicitação ao servidor deveria ser feita
+    }));
+
+    it('should load items from server', function() {
+        // Inicialmente, antes de o servidor responder,
+        // items deverá estar vazio
+        expect(ctrl.items).toEqual([]);
+
+        // Simula uma resposta do servidor
+        mockBackend.flush();
+
+        expect(ctrl.items).toEqual([{id: 1, label: 'Mock'}]);
+    });
+
+    afterEach(function() {
+        // Garante que todos os expects definidos em $httpBackend realmente foram chamadas
+        mockBackend.verifyNoOutstandingExpectation();
+
+        // Garante que todas as solicitações ao servidor 
+        // foram realmente respondidas (usando flush())
+        mockBackend.verifyNoOutstandingRequest();
+    });
+});
